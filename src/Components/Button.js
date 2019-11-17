c35fe8c7f1dbcd7d9fdf457e8057da72fc1623fe
Goutham Subramanyam <goutham@surveysparrow.com>
2019-11-17 17:51:43 +0530

    SCSS Structuring and Setting up Base Styles

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Button(props) {
   return (
-    <div>
-      <button className="one-button">{props.text}</button>
-    </div>
+    <button className="xp-button--primary">{props.text}</button>
   )
 }
