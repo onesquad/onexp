5de0e63828a8002dea80011a5a8986f2b8d104c9
Vinay Chandran <vinaychandranvs@gmail.com>
2019-11-20 12:24:07 +0530

    Added shadow and translate on hover to buttons

--- example/src/App.js
+++ example/src/App.js
@@ -1,83 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { Button, Text } from 'onexp'
+import { Button, Text } from "onexp";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div className='fx-row fx-ai--start xp--wrapper'>
-        <div className='xp--sidebar fx-column fx-ai--center wp--sm'>
-          <Text size='h1' color='white'>OneSquad</Text>
+      <div className="fx-row fx-ai--start xp--wrapper">
+        <div className="xp--sidebar fx-column fx-ai--center wp--sm">
+          <Text size="h1" color="white">
+            OneSquad
+          </Text>
         </div>
-        <div className='fx-grow-1 wptb--sm wplr--lg'>
-          <Text size='jumbo' weight='bold' color='black' extraClass='mb--lg'>Buttons</Text>
-          <div className='fx-row mb--lg'>
+        <div className="fx-grow-1 wptb--sm wplr--lg">
+          <Text size="jumbo" weight="bold" color="black" extraClass="mb--lg">
+            Buttons
+          </Text>
+          <div className="fx-row mb--lg">
+            <Button type="primary" text="Primary Button" extraClass="mr--lg" />
             <Button
-              type='primary'
-              text='Primary Button'
-              extraClass='mr--lg'
+              type="primary"
+              text="Button with Icon"
+              extraClass="mr--lg"
+              icon="calendar"
             />
             <Button
-              type='primary'
-              text='Button with Icon'
-              extraClass='mr--lg'
-              icon='calendar'
-            />
-            <Button
-              type='primary'
-              text='Primary Button'
-              extraClass='mr--lg'
-              loading
-            />
-          </div>
-          <div className='fx-row mb--lg'>
-            <Button
-              type='dark'
-              text='Dark Button'
-              extraClass='mr--lg'
-            />
-            <Button
-              type='dark'
-              text='Button with Icon'
-              extraClass='mr--lg'
-              icon='calendar'
-            />
-            <Button
-              type='dark'
-              text='Dark Button'
-              extraClass='mr--lg'
-              loading
-            />
-          </div>
-          <div className='fx-row mb--lg'>
-            <Button
-              type='light'
-              text='Light Button'
-              extraClass='mr--lg'
-            />
-            <Button
-              type='light'
-              text='Button with Icon'
-              extraClass='mr--lg'
-              icon='calendar'
-            />
-            <Button
-              type='light'
-              text='Light Button'
-              extraClass='mr--lg'
+              type="primary"
+              text="Primary Button"
+              extraClass="mr--lg"
               loading
             />
           </div>
           <div className="fx-row mb--lg">
-            <Text size="h4" color="black" weight="normal" extraClass="mr--sm">Icon Button:</Text>
+            <Button type="dark" text="Dark Button" extraClass="mr--lg" />
             <Button
-              type="icon"
+              type="dark"
+              text="Publish"
+              extraClass="mr--lg"
+              icon="cloud-upload"
+            />
+            <Button
+              type="dark"
+              text="Dark Button"
+              extraClass="mr--lg"
+              loading
+            />
+          </div>
+          <div className="fx-row mb--lg">
+            <Button type="light" text="Light Button" extraClass="mr--lg" />
+            <Button
+              type="light"
+              text="Button with Icon"
               extraClass="mr--lg"
               icon="calendar"
             />
+            <Button
+              type="light"
+              text="Light Button"
+              extraClass="mr--lg"
+              loading
+            />
+          </div>
+          <div className="fx-row mb--lg">
+            <Text size="h4" color="black" weight="normal" extraClass="mr--sm">
+              Icon Button:
+            </Text>
+            <Button type="icon" extraClass="mr--lg" icon="calendar" />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
