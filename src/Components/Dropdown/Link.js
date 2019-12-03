3e1892ffc9d07428678a7c83e95f106bb0c1b2f5
Vinay Chandran <vinaychandranvs@gmail.com>
2019-12-03 21:18:05 +0530

    v0.1.15

--- src/Components/Dropdown/Link.js
+++ src/Components/Dropdown/Link.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function({ children, onClick }) {
+export default function({ children, onClick, extraClass = "" }) {
   return (
-    <div onClick={onClick} className="xp-dropdown-link">
+    <div onClick={onClick} className={`xp-dropdown-link ${extraClass}`}>
       {children}
     </div>
   );
