e5e3dffc7a580b347015fdde440971fe688afc61
Vinay Chandran <vinaychandranvs@gmail.com>
2019-12-03 12:20:53 +0530

    Preserve aspect ratio of avatars, change font

--- example/src/Components/Avatars.js
+++ example/src/Components/Avatars.js
@@ -12,22 +12,22 @@ const Avatars = () => {
         <Avatar
           extraClass="mr--md"
           size="sm"
-          image="https://avatars1.githubusercontent.com/u/199754?s=460&v=4"
+          image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6"
         />
         <Avatar
           extraClass="mr--md"
           size="md"
-          image="https://avatars1.githubusercontent.com/u/199754?s=460&v=4"
+          image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6"
         />
         <Avatar
           extraClass="mr--md"
           size="lg"
-          image="https://avatars1.githubusercontent.com/u/199754?s=460&v=4"
+          image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6"
         />
         <Avatar
           extraClass="mr--md"
           size="xl"
-          image="https://avatars1.githubusercontent.com/u/199754?s=460&v=4"
+          image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6"
         />
       </div>
     </div>
